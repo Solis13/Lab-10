186810a054fd8c95406c7ad90de82db6985710d7
Solis13 <156252616+Solis13@users.noreply.github.com>
2024-03-28 00:22:17 -0500

    Lab10

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+
+import { ref } from 'vue'
+const bmi = ref('')
+const bmiHeight = ref('')
+const bmiWeight = ref('')
+
+ 
+ const calculateBMI = (heigth,weight,bim)=>{
+  bmi.value = heigth,weight,bim
+ }
+</script>
+
+<template>
+
+ <div id="app-component"> 
+
+    <h1>Body Mass Index Calculator</h1>
+    
+  
+
+    <BodyMassIndexForm
+    v-bind:bmi = "bmiHeight"
+    v-bind:heigth = "bmiHeight"
+    v-bind:weight="bmiWeight"
+    v-on:stats-entered="calculateBMI"/>
+
+ 
+   <p> {{ bmi }} </p>
+</div>
+
+ 
+
+</template>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/BodyMassIndexForm.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps ({
+  height: String,
+  weight: String,
+  bmi: String
+
+})
+
+
+const emit = defineEmits([
+  'stats-entered'
+
+])
+const height = ref('0')
+const weight = ref('0')
+
+const bmi = ref('weight / ( height * height')
+
+
+function statsEntered(){
+ // alert('do the math height' + height)
+  emit('stats-entered', height.value,weight.value, bmi.value)
+  
+}
+</script>
+
+<template>
+
+<label>Height in meters </label>
+<input v-model ="height">
+
+<p>
+<label>Weight in kilograms</label>
+<input v-model = "weight">
+</p>
+
+
+<button v-on:click ="statsEntered">Calculate</button>  
+  
+</template>
+
+<style scoped>
+
+button {
+  background-color: aquamarine;
+  margin: 10px;
+}
+
+
+</style>
